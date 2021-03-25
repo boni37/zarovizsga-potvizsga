6186e54ef155da40777c6bd3d85586af0877b569
boni37 <73605283+boni37@users.noreply.github.com>
2021-03-25 16:59:11 +0100

    Words

--- src/main/java/hu/nive/ujratervezes/zarovizsga/words/Words.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/words/Words.java
@@ -1,6 +1,16 @@
 package hu.nive.ujratervezes.zarovizsga.words;
 
 public class Words {
+
+    private String countPattern = "0123456789";
+
     public boolean hasMoreDigits(String s) {
+        int count = 0;
+        for (char ch : s.toCharArray()) {
+            if (countPattern.contains(String.valueOf(ch))) {
+                count += 1;
+            }
+        }
+        return count > s.length() / 2;
     }
 }
