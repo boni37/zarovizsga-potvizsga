53d3bdb03451e4648bbb767392c53bea76187383
boni37 <73605283+boni37@users.noreply.github.com>
2021-03-27 15:45:36 +0100

    PeopleDao

--- src/main/java/hu/nive/ujratervezes/zarovizsga/peoplesql/PeopleDao.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/peoplesql/PeopleDao.java
@@ -1,6 +1,32 @@
 package hu.nive.ujratervezes.zarovizsga.peoplesql;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class PeopleDao {
-    public String findIpByName(String brina, String snibson) {
+
+    private DataSource dataSource;
+
+    public PeopleDao(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public String findIpByName(String firstName, String lastName) {
+        String ip_address = "";
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement("select first_name, last_name, ip_address from people where first_name=? and last_name=? ");
+            statement.setString(1, firstName);
+            statement.setString(2, lastName);
+            ResultSet rs = statement.executeQuery();
+            if (rs.next()) {
+                ip_address = rs.getString("ip_address");
+            }
+        } catch (SQLException sqlException) {
+            throw new IllegalStateException("Can not read data", sqlException);
+        }
+        return ip_address;
     }
 }
